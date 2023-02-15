eceb3417a3effd3108e5cbda3e0567c3e6cfa9f6
Francisco jaque <fjaque330@gmail.com>
2023-02-14 21:24:26 -0300

    estilo

--- src/Components/Barra.jsx
+++ src/Components/Barra.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 const Barra = () => {
   return (
     <Navbar>
-      <Container bg="light">
-         <Link to="/"><Navbar.Brand>Home</Navbar.Brand></Link> 
+      <Container  style={{ display:'flex', justifyContent:'center'}} bg="light">
+         <Link to="/"><Navbar.Brand >Home</Navbar.Brand></Link> 
          <Link to='/favoritos'><Navbar.Brand>Favoritos</Navbar.Brand></Link> 
       </Container>
   </Navbar>
